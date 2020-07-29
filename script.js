6be52d09ed6ac7542745facee19ae5338fc577eb
Iuliia Siriakivska <60543102+SpaceSorceress@users.noreply.github.com>
2020-07-29 17:22:40 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,120 +1,127 @@
-let doorImage1=document.getElementById('door1');
-let doorImage2=document.getElementById('door2');
-let doorImage3=document.getElementById('door3');
-const startButton=document.getElementById('start');
-const botDoorPath='https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg';
-const beachDoorPath='https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg';
-const spaceDoorPath='https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg';
-const closedDoorPath='https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg';
+let doorImage1 = document.getElementById("door1");
+let doorImage2 = document.getElementById("door2");
+let doorImage3 = document.getElementById("door3");
+const startButton = document.getElementById("start");
+const botDoorPath =
+  "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+const beachDoorPath =
+  "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/beach.svg";
+const spaceDoorPath =
+  "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/space.svg";
+const closedDoorPath =
+  "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/closed_door.svg";
 
-
-let currentlyPlaying=true;
-const door1Clicked=()=>{
-    if(!isClicked(door1)&&currentlyPlaying){
-    doorImage1.src=openDoor1;
-    playDoor(door1);}
-    };
-
-door1.addEventListener('click',door1Clicked);
-
-door2.addEventListener('click',()=>{
-  if(!isClicked(door2)&&currentlyPlaying){
-doorImage2.src=openDoor2;
-playDoor(door2);}
-});
-door3.addEventListener('click',()=>{
-  if(!isClicked(door3)&&currentlyPlaying){
-doorImage3.src=openDoor3;
-playDoor(door3);}
-});
-
-
-startButton.onclick=()=>{
-    startRound();
+let currentlyPlaying = true;
+const door1Clicked = () => {
+  if (!isClicked(door1) && currentlyPlaying) {
+    doorImage1.src = openDoor1;
+    playDoor(door1);
+  }
 };
 
-const startRound=()=>{
-    startButton.innerHTML='Good luck!'
-    currentlyPlaying=true;
-    numClosedDoors=3;
-    door1.src=closedDoorPath;
-    door2.src=closedDoorPath;
-    door3.src=closedDoorPath;
-    randomChoreDoorGenerator();
-  };
+door1.addEventListener("click", door1Clicked);
 
+door2.addEventListener("click", () => {
+  if (!isClicked(door2) && currentlyPlaying) {
+    doorImage2.src = openDoor2;
+    playDoor(door2);
+  }
+});
+door3.addEventListener("click", () => {
+  if (!isClicked(door3) && currentlyPlaying) {
+    doorImage3.src = openDoor3;
+    playDoor(door3);
+  }
+});
 
-
-const gameOver=(status)=>{
-if(status==='win'){
-  startButton.innerHTML='You win! Play again?';
-}else{
-  startButton.innerHTML='Game over! Play again?';
-}
-currentlyPlaying=false;
+startButton.onclick = () => {
+  startRound();
 };
 
+const startRound = () => {
+  startButton.innerHTML = "Good luck!";
+  currentlyPlaying = true;
+  numClosedDoors = 3;
+  door1.src = closedDoorPath;
+  door2.src = closedDoorPath;
+  door3.src = closedDoorPath;
+  randomChoreDoorGenerator();
+};
 
-let numClosedDoors=3;
+const gameOver = (status) => {
+  if (status === "win") {
+    startButton.innerHTML = "You win! Play again?";
+  } else {
+    startButton.innerHTML = "Game over! Play again?";
+  }
+  currentlyPlaying = false;
+};
+
+let numClosedDoors = 3;
 let openDoor1;
 let openDoor2;
 let openDoor3;
 let choreDoor;
 
 const randomChoreDoorGenerator = () => {
-    choreDoor = Math.floor(Math.random() * 6);
-    switch (choreDoor) {
-      case 0:
-        openDoor1 = botDoorPath;
-        openDoor2 = beachDoorPath;
-        openDoor3 = spaceDoorPath;
-        break;
-      case 1:
-        openDoor1 = botDoorPath;
-        openDoor2 = spaceDoorPath;
-        openDoor3 = beachDoorPath;
-        break;
-      case 2:
-        openDoor2 = botDoorPath;
-        openDoor1 = beachDoorPath;
-        openDoor3 = spaceDoorPath;
-        break;
-      case 3:
-        openDoor2 = botDoorPath;
-        openDoor1 = spaceDoorPath;
-        openDoor3 = beachDoorPath;
-        break;
-      case 4:
-        openDoor3 = botDoorPath;
-        openDoor1 = beachDoorPath;
-        openDoor2 = spaceDoorPath;
-        break;
-      case 5:
-        openDoor3 = botDoorPath;
-        openDoor1 = spaceDoorPath;
-        openDoor2 = beachDoorPath;
-        break;
-    }
-  };
-
-  
-const isBot=(door)=>{
-if(door.src===botDoorPath){return true;}else{return false;}
-}
-
-const isClicked=(door)=>{
-if(door.src===closedDoorPath){
-  return false}else{return true;}
+  choreDoor = Math.floor(Math.random() * 6);
+  switch (choreDoor) {
+    case 0:
+      openDoor1 = botDoorPath;
+      openDoor2 = beachDoorPath;
+      openDoor3 = spaceDoorPath;
+      break;
+    case 1:
+      openDoor1 = botDoorPath;
+      openDoor2 = spaceDoorPath;
+      openDoor3 = beachDoorPath;
+      break;
+    case 2:
+      openDoor2 = botDoorPath;
+      openDoor1 = beachDoorPath;
+      openDoor3 = spaceDoorPath;
+      break;
+    case 3:
+      openDoor2 = botDoorPath;
+      openDoor1 = spaceDoorPath;
+      openDoor3 = beachDoorPath;
+      break;
+    case 4:
+      openDoor3 = botDoorPath;
+      openDoor1 = beachDoorPath;
+      openDoor2 = spaceDoorPath;
+      break;
+    case 5:
+      openDoor3 = botDoorPath;
+      openDoor1 = spaceDoorPath;
+      openDoor2 = beachDoorPath;
+      break;
+  }
 };
 
+const isBot = (door) => {
+  if (door.src === botDoorPath) {
+    return true;
+  } else {
+    return false;
+  }
+};
 
-const playDoor=(door)=>{
+const isClicked = (door) => {
+  if (door.src === closedDoorPath) {
+    return false;
+  } else {
+    return true;
+  }
+};
+
+const playDoor = (door) => {
   numClosedDoors--;
-  if(numClosedDoors === 0){
-    gameOver('win');
-  }else if(isBot(door)){
+  if (numClosedDoors === 0) {
+    gameOver("win");
+  } else if (isBot(door)) {
     gameOver();
-  };
+  }
 };
 
 startRound();
